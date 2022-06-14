6304e576d13791b92dcddb46ab284dabbc6c5a0f
Thomas Anton <thomasanton1224@gmail.com>
2022-06-14 17:43:03 -0400

    phase 2 complete

--- server/app.js
+++ server/app.js
@@ -7,6 +7,16 @@ const path = require('node:path')
 app.use('/static', express.static('assets'))
 app.use(express.json())
 
+app.use((req, res, next) => {
+  console.log('Method: ', req.method)
+  console.log('Path: ', req.path)
+  res.on('finish', () => {
+    console.log(res.statusCode)
+  })
+  next()
+})
+
+
 // For testing purposes, GET /
 app.get('/', (req, res) => {
   res.json("Express server running. No content provided at root level. Please use another route.");
@@ -24,6 +34,18 @@ app.post('/test-json', (req, res, next) => {
 app.get('/test-error', async (req, res) => {
   throw new Error("Hello World!")
 });
+
+app.use((req, res, next) => {
+  const err = new Error ("The requested resource couldn't be found.")
+  err.statusCode = 404
+  next(err)
+})
+
+app.use((err, req, res, next) => {
+  console.log("Error: Not Found")
+  res.send("The requested resource could not be found.")
+  res.statusCode = err.statusCode
+})
 
 const port = 8000;
 app.listen(port, () => console.log('Server is listening on port', port));
